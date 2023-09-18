f9d15ddb328dfe450bb09b6e3e52eb6f1bc9a0f8
Mitch Y <mitchellsimy@gmail.com>
2023-09-18 10:12:45 -0500

    Updated Accordion and Buttons

--- accordion/script.js
+++ accordion/script.js
@@ -1,19 +1,16 @@
+let accordionHeaders = document.querySelectorAll('.accordion-header');
+accordionHeaders.forEach(header => {
 
-function openDrawer() {
-    const accordionHeaders = document.querySelectorAll('.hc-accordion-header');
-    console.log(accordionHeaders)
-    accordionHeaders.forEach(header => {
-        header.addEventListener('click', () => {
-            header.classList.toggle('active');
-            const symbol = header.querySelector('.hc-accordion-symbol');
-            symbol.textContent = header.classList.contains('active') ? "-" : "+";
-            const content = header.nextElementSibling;
-            if (content.style.display === 'block') {
-                content.style.display = 'none';
-            } else {
-                content.style.display = 'block';
-            }
-        });
+    header.addEventListener('click', () => {
+        header.classList.toggle('active');
+        const symbol = header.querySelector('.symbol');
+        symbol.textContent = header.classList.contains('active') ? "-" : "+";
+        const content = header.nextElementSibling;
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+        } else {
+            content.style.display = 'block';
+        }
     });
-
-}
+});
+accordionHeaders = null
