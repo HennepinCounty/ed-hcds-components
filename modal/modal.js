8e78a28e0121ce6ade4e78cc073dd78e6b53ebdf
JoeyPearlmanHennepin <joey.pearlman@hennepin.us>
2023-12-01 14:55:27 -0600

    updated margint utility classes to be in root for css specificity

--- modal/modal.js
+++ modal/modal.js
@@ -1,16 +1,16 @@
-const openButton = document.querySelector('[data-open-modal]');
-        const closeButton = document.querySelector('[data-close-modal]');
-        const modal = document.querySelector('[data-modal]');
-        const iconClose = document.querySelector('[icon]');
+const openButton = document.querySelector("[data-open-modal]");
+const closeButton = document.querySelector("[data-close-modal]");
+const modal = document.querySelector("[data-modal]");
+const iconClose = document.querySelector("[icon]");
 
-        openButton.addEventListener('click', () =>{
-            modal.show();
-        });
+openButton.addEventListener("click", () => {
+  modal.show();
+});
 
-        closeButton.addEventListener('click', () =>{
-            modal.close();
-        });
+closeButton.addEventListener("click", () => {
+  modal.close();
+});
 
-        iconClose.addEventListener('click', () =>{
-            modal.close();
-        });
+iconClose.addEventListener("click", () => {
+  modal.close();
+});
