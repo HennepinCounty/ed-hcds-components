83e4c3e00a1b5b0fa04e9a27a5d4b2c0f3b56733
JoeyPearlmanHennepin <joey.pearlman@hennepin.us>
2023-12-04 15:55:43 -0600

    imported checkbox changes

---
+++ checkboxes/checkbox.js
@@ -0,0 +1,27 @@
+function onClick(event) {
+  event.preventDefault();
+}
+
+function toggleDisabled(element, status, update) {
+  if (status) {
+    element.disabled = false;
+    element.setAttribute("aria-disabled", "false");
+    update.textContent = "The checkbox is now enabled.";
+    element.addEventListener("click", onClick);
+  } else {
+    element.disabled = true;
+    element.setAttribute("aria-disabled", "true");
+    update.textContent = "The checkbox is now disabled.";
+    element.removeEventListener("click", onClick);
+  }
+}
+
+// Call toggleDisabled for all checkboxes with aria-disabled="true"
+const disabledCheckboxes = document.querySelectorAll(
+  'input[aria-disabled="true"]'
+);
+const updateElement = document.getElementById("updateMessage"); // Assuming you have an element to display the update message
+
+disabledCheckboxes.forEach((checkbox) => {
+  toggleDisabled(checkbox, true, updateElement);
+});
