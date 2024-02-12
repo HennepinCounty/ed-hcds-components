996b30a8c70a2b01b75951fef0e658940c0e1d66
SusannahHarris-HC <susannah.harris@hennepin.us>
2024-02-12 16:33:40 -0600

    updated alert banner with alert.js for closing button and focus post-close

---
+++ components/alertBanner/alert.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const closeButtons = document.querySelectorAll('.hc-alert-close-button');
+    
+    closeButtons.forEach(function(button) {
+        button.addEventListener('click', function() {
+            const alertContainer = this.closest('.hc-alert');
+            if (alertContainer) {
+                alertContainer.style.display = 'none';
+
+                // Use this to set focus to a predetermined element after closing the alert
+                // Usually the element that triggered the alert if applicable
+                document.getElementById('main-content-heading').focus();
+            }
+        });
+    });
+});
