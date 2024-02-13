d26e42db0d56e24f412fad53fe1bd3b9de711176
SusannahHarris-HC <susannah.harris@hennepin.us>
2024-02-13 10:25:14 -0600

    modified comments in modal.js

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -1,7 +1,3 @@
-//open and close modal
-//sets focus within the modal
-//prevent scrolling outside of modal
-
 const openButton = document.querySelector("[data-open-modal]");
 const modal = document.querySelector("[data-modal]");
 let lastFocusedElement;
@@ -9,27 +5,30 @@ let lastFocusedElement;
 // Selects all elements that can close the modal
 const closeButtons = document.querySelectorAll("[data-close-modal]");
 
-openButton.addEventListener("click", () => {
 
-  // Save the current focus before opening the modal
+// when modal opens, focus is saved on element that opened modal
+// and scrolling prevented in background
+openButton.addEventListener("click", () => {
   lastFocusedElement = document.activeElement; 
   modal.showModal();
-
-  // Optionally, set focus to a specific element within the modal here
-  // Prevent scrolling on the background
   document.body.style.overflow = 'hidden'; 
 });
 
+
+// when modal closes, focus is on element that opened modal
+// restores scrolling on background
 const closeModal = () => {
   modal.close();
-  if (lastFocusedElement) lastFocusedElement.focus(); // Restore focus to the element that opened the modal
-  document.body.style.overflow = ''; // Restore scrolling on the background
+  if (lastFocusedElement) lastFocusedElement.focus(); 
+  document.body.style.overflow = ''; 
 };
+
 
 // Attach the closeModal function to all close buttons
 closeButtons.forEach(button => {
   button.addEventListener("click", closeModal);
 });
+
 
 // Handle the Escape key to close the modal
 document.addEventListener("keydown", (e) => {
